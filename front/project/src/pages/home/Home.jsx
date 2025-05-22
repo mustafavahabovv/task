afc1f58baa0cfbcf87912c0caeb9c61b3297f754
mustafavahabovv <mustafajv-af107@code.edu.az>
2025-05-22 22:11:24 +0400

    first commit

---
+++ front/project/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Home.module.css'
+import WelcomeSection from './components/welcomesection/WelcomeSection'
+
+function Home() {
+  return (
+    <div className={style.home}>
+      
+    </div>
+  )
+}
+
+export default Home
